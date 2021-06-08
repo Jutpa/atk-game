b282f5ebd4acaa0d0a9886cba21c75ca75d5c5f7
Jutpa <84371110+Jutpa@users.noreply.github.com>
2021-06-08 17:52:07 +1000

    The Unity Project Thing

--- Au To Know Game/Assets/Scripts/FollowCamera.cs
+++ Au To Know Game/Assets/Scripts/FollowCamera.cs
@@ -9,9 +9,9 @@ public class FollowCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
-
+    
     // Update is called once per frame
     void Update()
     {
--- Au To Know Game/Assets/Scripts/ThirdPersonController.cs
+++ Au To Know Game/Assets/Scripts/ThirdPersonController.cs
@@ -7,17 +7,18 @@ public class ThirdPersonController : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    public float playerSpeed = 2.0f;
+    public float playerSpeed = 1.0f;
+    public float playerRot = 1.0f;
     public float jumpHeight = 1.0f;
     public float gravityValue = -9.81f;
     public float health = 100.0f;
     public float maxHealth = 100.0f;
-
+    
     private void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
     }
-
+    
     void Update()
     {
         groundedPlayer = controller.isGrounded;
@@ -26,20 +27,20 @@ public class ThirdPersonController : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        float translation = Input.GetAxis("Vertical") * playerSpeed;
+        float rotation = Input.GetAxis("Horizontal") * playerRot;
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+        rotation *= Time.deltaTime;
 
+        transform.Rotate(0, rotation, 0);
+        playerVelocity.z = translation;
+        
         // Changes the height position of the player.
         if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
-
+        
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
