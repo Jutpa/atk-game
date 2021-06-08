4c66b411c54e1114ef1f9a7bd51c4ed54b54d079
Jutpa <84371110+Jutpa@users.noreply.github.com>
2021-06-08 16:17:00 +1000

    The Unity Project Thing

---
+++ Au To Know Game/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public GameObject cameraTarget;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraTarget.transform.position, 0.01f);
+        transform.LookAt(player.transform);
+    }
+}
